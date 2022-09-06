b8560e112cab6e7a5f92371a963f4d6e80b46285
paulrndp <paulr@DESKTOP-47REJSO>
2022-09-06 16:10:20 +0800

    Add project files.

---
+++ IdentityAuth/Program.cs
@@ -0,0 +1,61 @@
+using IdentityAuth.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication().
+    AddGoogle(options =>{
+    IConfigurationSection googleAuthSection = builder.Configuration.GetSection("Authentication:Google");
+
+    options.ClientId = googleAuthSection["ClientId"];
+    options.ClientSecret = googleAuthSection["ClientSecret"];
+
+       
+});
+builder.Services.AddAuthentication().
+AddFacebook(options => {
+    IConfigurationSection googleAuthSection = builder.Configuration.GetSection("Authentication:Facebook");
+
+    options.AppId = googleAuthSection["AppId"];
+    options.AppSecret = googleAuthSection["AppSecret"];
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
